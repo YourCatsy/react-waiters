ac3cee368432f0d9f6a10976e0d3be475abce3b4
Catherine AMF <catherineamf@gmail.com>
2024-07-03 15:48:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Endpoint for health check
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Health check endpoint
 app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+  res.status(200).send('OK');
 });
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Catch all handler to return the React app for any request not handled by the above
+// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
